fe08330007bdb57bf13293eb5323e512aae35aff
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-28 15:46:54 +0430

    Add files via upload

---
+++ Two-Sum_And_Palindrome-Number/Implement.cs
@@ -0,0 +1,55 @@
+﻿namespace Two_Sum_And_Palindrome_Number
+{
+    public class Implement
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            else
+            {
+                int revers = 0;
+                int num = x;
+                int remein;
+                while (num > 0)
+                {
+                    remein = num % 10;
+                    num /= 10;
+                    revers = (revers * 10) + remein;
+                }
+                if (revers == x)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int firstIndext = 0;
+            int secondIndext = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        firstIndext = i;
+                        secondIndext = j;
+                        break;
+                    }
+                }
+            }
+            int[] result = new int[] { firstIndext, secondIndext };
+            return result;
+        }
+    }
+}
+
---
+++ Two-Sum_And_Palindrome-Number/Program.cs
@@ -0,0 +1,5 @@
+﻿using Two_Sum_And_Palindrome_Number;
+
+Implement a = new Implement();
+
+Console.WriteLine(a.IsPalindrome(121));
